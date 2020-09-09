1b7f0a0cbd3a35e364590db73961d4514edc024a
sheiler <silvio.heiler@gmail.com>
2020-09-08 23:18:51 -0300

    1er commit

---
+++ ConsoleApp6/ConsoleApp6/Auto.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp6
+{
+    public class Auto
+    {
+
+
+        private string marca;
+        private string modelo;
+        private string version;
+        private int stock;
+        private bool estado;
+
+        public string Marca { 
+            get { return marca; } 
+            set { marca = value; } 
+        }
+        public string Modelo
+        {
+            get { return modelo; }
+            set { 
+                if(this.marca == "ford")
+                {
+                    if (value == "fiesta")
+                        modelo = value;
+                    else
+                        throw new Exception();
+                }
+                else
+                {
+                    modelo = value;
+                }
+                    
+            }
+        }
+        public string Version //read only
+        {
+            get { return version; }
+            
+        }
+
+        public Auto() { }
+        public Auto(string marca, string modelo, string version)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.version = version;
+            this.stock = 10;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} - {2}", this.Marca, this.Modelo, this.Version);
+        }
+
+        public void AgregarUnaUnidad()
+        {
+            this.stock = this.stock+1;
+        }
+
+        public void QuitarStock(int cantidad)
+        {
+            this.stock = this.stock - cantidad;
+        }
+
+        /// <summary>
+        /// En caso de querer desactivar enviar false
+        /// </summary>
+        /// <param name="cantidad"></param>
+        /// <param name="desactivar"></param>
+        public void QuitarStock(int cantidad, bool desactivar)
+        {
+            this.stock = this.stock - cantidad;
+
+            if (desactivar)
+                estado = false;
+        }
+
+        public bool Vendido()
+        {
+            if (stock == 0)
+                return true;
+            else
+                return false;
+
+            
+        }
+
+        public void HagoAlgo()
+        {
+            // NO PUEDE HABER ACCESO A LA CAPA DE PRESENTACION DESDE UNA ENTIDAD
+            Console.WriteLine("Hago algo");
+        }
+
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+
+        
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Auto> listaAutos = new List<Auto>();
+
+            Auto a1 = new Auto("Ford", "Fiesta", "Nafta 1.4 TD");
+            
+            listaAutos.Add(a1);
+
+            Auto a2 = null;
+
+            a2.Marca = "123123";
+            //Console.WriteLine(a2.Marca);
+
+            a2 = new Auto("Chevrolet", "Onix", "Nafta 1.3 TD");
+
+            Console.WriteLine(a2.Marca);
+
+            a2.AgregarUnaUnidad();
+                
+            listaAutos.Add(a2);
+                
+            listaAutos.Add(new Auto("Volskwagen", "Golf", "Nafta 1.4 TSI"));
+                
+            Console.WriteLine("Ingrese el nombre de un auto");
+
+            Auto a = new Auto();
+            
+            Auto a3 = a2;
+
+            a2.QuitarStock(11);
+
+            a3.AgregarUnaUnidad();
+
+            foreach (Auto auto in listaAutos)
+            {
+                Console.WriteLine(auto.ToString());
+
+                if (auto.Vendido())
+                    Console.WriteLine("Esta vendido");
+                else
+                    Console.WriteLine("Hay stock");
+
+            //if (auto.Modelo.ToUpper().Contains(ingresoUsuario.ToUpper()))
+            //{
+            //    Console.WriteLine(auto.ToString());
+            //}
+            }
+
+                string ingresoUsuario = Console.ReadLine();
+        }
+
+        public static void GetDesc(Auto a)
+        {
+            Console.WriteLine(a.Marca);
+        }
+    }
+    
+    
+}
